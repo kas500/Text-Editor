ebd55e7ca829aef2fd0e02f5cda7dd6f4c22ee19
Anton Krasnikov <krasnikovanton84@gmail.com>
2023-02-20 17:24:11 -0800

    Adds WebpackPwaManifest plugin

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -30,6 +30,27 @@ module.exports = () => {
         swDest: 'src-sw.js',
       }),
 
+      // Creates a manifest.json file.
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Text Editor',
+        short_name: 'Editor',
+        description: 'Another text editor',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
+
+
     ],
 
     
