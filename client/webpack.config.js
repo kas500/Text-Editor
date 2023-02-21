d456f22f570b07a8104082d38e0d97c332df94ed
Anton Krasnikov <krasnikovanton84@gmail.com>
2023-02-20 17:15:24 -0800

    Adds HtmlWebpackPlugin

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,13 +18,14 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+       // Webpack plugin that generates our html file and injects our bundles. 
+       new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Text Editor'
+      }),
+     
     ],
 
-    module: {
-      rules: [
-        
-      ],
-    },
+    
   };
 };
